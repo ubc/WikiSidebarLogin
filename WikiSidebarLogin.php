4372631f5a1c09fff20fcef59102a9e6450c8a30
System Administrator <sysadmin@wikiWebVerf11.webi.it.ubc.ca>
2014-08-25 16:01:08 -0700

    login has to be https and include referral

--- WikiSidebarLogin.php
+++ WikiSidebarLogin.php
@@ -2,8 +2,8 @@
 $wgExtensionCredits['validextensionclass'][] = array(
     'path' => __FILE__,
     'name' => 'Wiki Sidebar Login',
-    'author' => 'UBC CTLT', 
-    'url' => 'https://www.wiki.ubc.ca', 
+    'author' => 'UBC CTLT',
+    'url' => 'https://wiki.ubc.ca',
     'description' => 'Adds a login link to the sidebar for users who are not loggedin',
     'version'  => 1,
 );
@@ -11,14 +11,25 @@ $wgExtensionCredits['validextensionclass'][] = array(
 
 $wgHooks['SkinBuildSidebar'][] = 'lfHideSidebar';
 function lfHideSidebar( $skin, &$bar ) {
-    global $wgUser;
+    global $wgUser, $wgServerName, $wgTitle;
     // Hide sidebar for anonymous users
     if ( !$wgUser->isLoggedIn() ) {
+
+        $currentTitle  =  $wgTitle->getPrefixedText();
+
+        if( !preg_match('/UserLogin/',$currentTitle)  ){
+                $sb_referral .= $currentTitle;
+        }else{
+                $sb_referral .= "Main_Page";
+        }
+
+
+
         $bar = array(
             'navigation' => array(
                 array(
                     'text'   => wfMsg( 'login' ),
-                    'href'   => SpecialPage::getTitleFor( 'Login' )->getLocalURL(),
+                    'href' => "https://$wgServerName/index.php?title=Special:UserLogin&returnto=$sb_referral",
                     'id'     => 'n-login',
                     'active' => ''
                 )
